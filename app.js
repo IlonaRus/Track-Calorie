8cdfbc8f3b82d78647e74320638ff8e87983b061
Ilona <ilonarus@hotmail.com>
2021-03-06 16:55:49 +0100

    Created UISelector for use in populateItemList()

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ const ItemCtrl = (function() {
 
 // UI Controller
 const UICtrl = (function() {
+  const UISelectors =  {
+    itemList: '#item-list'
+  }
 
   // Public methods
   return {
@@ -52,7 +55,7 @@ const UICtrl = (function() {
       });
 
       // Insert list items
-      document.querySelector('#item-list').innerHTML = html;
+      document.querySelector(UISelectors.itemList).innerHTML = html;
     }
   }
 })();
